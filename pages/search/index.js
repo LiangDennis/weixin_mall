331824a803bc04482a331cf35e7d15cbc0bf7d3e
LiangDennis <18898513421@163.com>
2020-03-21 11:22:37 +0800

    搜索中心

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,53 @@
 // pages/search/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goods: [],
+    // 是否展示取消按钮
+    showBtn: false,
+    inputValue: ''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 定时器 节流
+  timeId: -1,
+  // 搜索框
+  handleInput(e) {
+    // 1. 获取值
+    const {value} = e.detail;
+    // 2. 合法性校验
+    if(!value.trim()) {
+      this.setData({
+        showBtn: false,
+        goods: []
+      });
+      return;
+    }
+    // 3. 发送请求
+    this.setData({
+      showBtn: true
+    });
+    clearTimeout(this.timeId);
+    this.timeId = setTimeout(() => {
+      this.qsearch(value);
+    }, 1000);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 点击清除
+  handleColse() {
+    this.setData({
+      inputValue: '',
+      showBtn: false,
+      goods: []
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 发送请求
+  async qsearch(query) {
+    const res = await request({url: '/goods/qsearch', data:{query}});
+    this.setData({
+      goods: res
+    });
   }
 })
